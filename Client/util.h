797971f2d184a9e0b8e3a29b2fe4ed23ae01d84c
Toyro98 <teddymolin1@gmail.com>
2023-06-03 18:42:48 +0200

    Keybinds Reset
    Added a button on each keybind to reset it back to default keybind. Moved input code to util.h, and changed the menu styling

--- Client/util.h
+++ Client/util.h
@@ -1,3 +1,5 @@
+#include <WinUser.h>
+
 #pragma once
 
 /*
@@ -47,4 +49,27 @@
 inline static float Distance(Classes::FVector from, Classes::FVector to) 
 { 
 	return sqrt(((from.X - to.X) * (from.X - to.X)) + ((from.Y - to.Y) * (from.Y - to.Y)) + ((from.Z - to.Z) * (from.Z - to.Z))) / 100;
+}
+
+static INPUT input = {0};
+
+inline static void PressKey(int keyCode) {
+    input.type = INPUT_KEYBOARD;
+    input.ki.wScan = 0;
+    input.ki.time = 0;
+    input.ki.dwExtraInfo = 0;
+
+    input.ki.wVk = keyCode;
+    input.ki.dwFlags = 0;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+inline static void ReleaseKey() {
+    input.type = INPUT_KEYBOARD;
+    input.ki.wScan = 0;
+    input.ki.time = 0;
+    input.ki.dwExtraInfo = 0;
+
+    input.ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, &input, sizeof(INPUT));
 }
