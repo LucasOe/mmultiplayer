3fdbdec597071628ea1ce88c3f741cee1a96ea9b
Toyro98 <teddymolin1@gmail.com>
2023-06-06 00:49:06 +0200

    Added Double Jump
    Allows you to double jump. Customize how many jumps you want. Being able to hide the overlay has been disabled

--- Client/addons/trainer.cpp
+++ Client/addons/trainer.cpp
@@ -8,19 +8,43 @@
 #include "../settings.h"
 #include "../util.h"
 
-static auto enabled = false, overlay = true, tooltip = true, god = false, kg = false,
-            beamer = false, sidestepBeamer = false, sidestepBeamerForMe = false, strang = false, 
-            resetFlyingSpeed = true, sidestepBeamerPressLeftKeybind = true,
-            sidestepBeamerPressRightKeybind = false, toggleResetKeybinds = false;
-
-static int saveKeybind = 0, loadKeybind = 0, godKeybind = 0, kgKeybind = 0, beamerKeybind = 0,
-           strangKeybind = 0, sidestepBeamerLeftKeybind = 0, sidestepBeamerRightKeybind = 0;
+static bool enabled = false;
+static bool tooltip = true;
+static bool god = false;
+static bool kg = false;
+static bool beamer = false;
+static bool strang = false; 
+static bool sidestepBeamer = false;
+static bool sidestepBeamerForMe = false;
+static bool sidestepBeamerPressLeftKeybind = true;
+static bool sidestepBeamerPressRightKeybind = false;
+static bool resetFlyingSpeed = true;
+static bool toggleResetKeybinds = false;
+static bool doubleJumpEnabled = false;
+static bool doubleJump = false;
+static int doubleJumpCount = 0;
+static int doubleJumpCountMax = 1;
+static int doubleJumpCountSaved = -1;
+static bool canDoubleJump = false;
+static int saveKeybind = 0;
+static int loadKeybind = 0;
+static int godKeybind = 0;
+static int kgKeybind = 0;
+static int beamerKeybind = 0;
+static int strangKeybind = 0;
+static int sidestepBeamerLeftKeybind = 0;
+static int sidestepBeamerRightKeybind = 0;
+static int doubleJumpKeybind = 0;
 
 static struct {
     bool Enabled = false;
-    int Keybind = 0, UpKeybind = 0, DownKeybind = 0, FasterKeybind = 0,
-        SlowerKeybind = 0;
-    Classes::FVector Location, Velocity;
+    int Keybind = 0;
+    int UpKeybind = 0;
+    int DownKeybind = 0;
+    int FasterKeybind = 0;
+    int SlowerKeybind = 0;
+    Classes::FVector Location;
+    Classes::FVector Velocity;
     float Speed = 2.0f;
     float DefaultSpeed = Speed;
 } fly;
@@ -327,6 +351,7 @@ static void TrainerTab() {
             beamer = false;
             strang = false;
             sidestepBeamer = false;
+            doubleJump = false;
             fly.Enabled = false;
 
             pawn->Velocity = fly.Velocity;
@@ -338,10 +363,6 @@ static void TrainerTab() {
 
     if (!enabled) {
         return;
-    }
-
-    if (ImGui::Checkbox("Overlay##trainer-overlay", &overlay)) {
-        Settings::SetSetting("trainer", "overlay", overlay);
     }
 
     if (ImGui::Checkbox("Tooltip##trainer-tooltip", &tooltip)) {
@@ -363,7 +384,7 @@ static void TrainerTab() {
     }
 
     if (tooltip && ImGui::IsItemHovered(ImGuiHoveredFlags_None)) {
-        ImGui::SetTooltip("Instead of it quickturning, it will jump");
+        ImGui::SetTooltip("Instead of it quickturning, it will jump.");
     } 
     
     if (sidestepBeamer) {
@@ -415,6 +436,26 @@ static void TrainerTab() {
                     Settings::SetSetting("trainer", "sidestepBeamerRightKeybind", sidestepBeamerRightKeybind = VK_D);
                 }
             }
+        }
+    }
+
+    ImGui::SeperatorWithPadding(2.5f);
+
+    if (ImGui::Checkbox("Double Jump##trainer-doubleJumpEnabled", &doubleJumpEnabled)) {
+        Settings::SetSetting("trainer", "doubleJumpEnabled", doubleJumpEnabled);
+    }
+
+    if (tooltip && ImGui::IsItemHovered(ImGuiHoveredFlags_None)) {
+        ImGui::SetTooltip("Allows you to double jump. Simply press the spacebar while you're in a falling state\nor watch the top right corner for \"Double Jump\"");
+    } 
+
+    if (doubleJumpEnabled) {
+        if (ImGui::InputInt("Max Jumps##trainer-doubleJumpCountMax", &doubleJumpCountMax, 1, 1, ImGuiInputTextFlags_EnterReturnsTrue)) {
+            Settings::SetSetting("trainer", "doubleJumpCountMax", doubleJumpCountMax = max(1, doubleJumpCountMax));
+        }
+
+        if (tooltip && ImGui::IsItemHovered(ImGuiHoveredFlags_None)) {
+            ImGui::SetTooltip("Set your own limit of how many times you can jump.\nDefault value: 1");
         }
     }
 
@@ -562,7 +603,7 @@ static void TrainerTab() {
 }
 
 static void OnRender(IDirect3DDevice9 *) {
-    if (!enabled || !overlay) {
+    if (!enabled) {
         return;
     }
 
@@ -571,6 +612,7 @@ static void OnRender(IDirect3DDevice9 *) {
     text += kg ? "KG " : "";
     text += fly.Enabled ? "Fly " : "";
     text += god ? "God " : "";
+    text += doubleJump && doubleJumpCount < doubleJumpCountMax && canDoubleJump && doubleJumpEnabled ? "Double Jump " : "";
 
     if (text != "") {
         static const auto padding = 5.0f;
@@ -607,6 +649,10 @@ static void OnTick(float deltaTime) {
     }
 
     if (pawn->Health <= 0) {
+        if (doubleJumpEnabled && doubleJumpCount > 0) {
+            doubleJumpCount = 0;
+            // printf("doubleJumpCount reset to %d (player died)\n", doubleJumpCountMax);
+        }
         return;
     }
 
@@ -614,6 +660,10 @@ static void OnTick(float deltaTime) {
     static auto hasSave = false;
 
     if (Engine::IsKeyDown(saveKeybind)) {
+        if (doubleJumpEnabled) {
+            doubleJumpCountSaved = doubleJumpCount;
+        }
+
         Save(save, pawn, controller);
         hasSave = true;
     }
@@ -621,6 +671,10 @@ static void OnTick(float deltaTime) {
     if (hasSave && Engine::IsKeyDown(loadKeybind)) {
         if (sidestepBeamer && sidestepBeamerForMe) {
             ReleaseKey();
+        }
+
+        if (doubleJumpEnabled) {
+            doubleJumpCount = doubleJumpCountSaved;
         }
 
         if (strang) {
@@ -721,6 +775,30 @@ static void OnTick(float deltaTime) {
             beamer = false;
         }
     }
+
+    if (doubleJumpEnabled) {
+        canDoubleJump = !pawn->Base && doubleJumpCount != doubleJumpCountMax && pawn->MovementState == Classes::EMovement::MOVE_Falling;
+
+        if (canDoubleJump && pawn->OldMovementState != Classes::EMovement::MOVE_WallClimbing) {
+            doubleJump = true;
+        } else {
+            doubleJump = false;
+        }
+
+        if (doubleJump && pawn->OldMovementState == Classes::EMovement::MOVE_WallClimbing) {
+            doubleJump = false;
+        }
+
+        if (doubleJumpCount > 0 && pawn->Base) {
+            doubleJumpCount = 0;
+            // printf("doubleJumpCount reset to %d (grounded)\n", doubleJumpCountMax);
+        }
+
+        if (doubleJumpCount > 0 && (pawn->MovementState == Classes::EMovement::MOVE_WallRunningRight || pawn->MovementState == Classes::EMovement::MOVE_WallRunningLeft)) {
+            doubleJumpCount = 0;
+            // printf("doubleJumpCount reset to %d (wallrunning)\n", doubleJumpCountMax);
+        }
+    }
 }
 
 void __fastcall StateHandlerHook(void *pawn, void *idle, float delta,
@@ -742,7 +820,6 @@ void __fastcall StateHandlerHook(void *pawn, void *idle, float delta,
 bool Trainer::Initialize() {
     // Settings
     enabled = Settings::GetSetting("trainer", "enabled", false);
-    overlay = Settings::GetSetting("trainer", "overlay", true);
     tooltip = Settings::GetSetting("trainer", "tooltip", true);
     saveKeybind = Settings::GetSetting("trainer", "saveKeybind", VK_4);
     loadKeybind = Settings::GetSetting("trainer", "loadKeybind", VK_5);
@@ -770,6 +847,11 @@ bool Trainer::Initialize() {
     sidestepBeamerPressLeftKeybind = Settings::GetSetting("trainer", "sidestepBeamerPressLeftKeybind", true);
     sidestepBeamerPressRightKeybind = Settings::GetSetting("trainer", "sidestepBeamerPressRightKeybind", false);
 
+    // Double Jump
+    doubleJumpEnabled = Settings::GetSetting("trainer", "doubleJumpEnabled", false);
+    doubleJumpKeybind = Settings::GetSetting("trainer", "doubleJumpKeybind", VK_SPACE);
+    doubleJumpCountMax = Settings::GetSetting("trainer", "doubleJumpCountMax", 1);
+
     // Functions
     Menu::AddTab("Trainer", TrainerTab);
     Engine::OnTick(OnTick);
@@ -792,6 +874,24 @@ bool Trainer::Initialize() {
         }
 
         if (msg == WM_KEYDOWN) {
+            if (keycode == doubleJumpKeybind && doubleJumpEnabled) {
+                const auto pawn = Engine::GetPlayerPawn();
+                const auto controller = Engine::GetPlayerController();
+
+                if (pawn) {
+                    if (doubleJumpCount < doubleJumpCountMax && canDoubleJump) {
+                        pawn->SetMove(Classes::EMovement::MOVE_Walking, false, false);
+                        controller->PlayerInput->Jump();
+
+                        doubleJump = false;
+                        doubleJumpCount++;
+
+                        // int jumpsLeft = doubleJumpCountMax - doubleJumpCount;
+                        // printf("doubleJump (%d jump%s left)\n", jumpsLeft, jumpsLeft != 1 ? "s" : "");
+                    }
+                }
+            }
+
             if (keycode == godKeybind) {
                 god = !god;
 
