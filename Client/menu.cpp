af7b4a08a9bb6dc5fa3dad134ca0d655ce145c8f
Toyro98 <teddymolin1@gmail.com>
2024-01-29 13:58:54 +0100

    Update menu.cpp
    If both sublevels had the same index, it then has a different number. This appends the number to the name and can be now be loaded and unloaded. However, this needs further testing

--- Client/menu.cpp
+++ Client/menu.cpp
@@ -117,7 +117,13 @@ static void WorldTab() {
 			auto level = levels.GetByIndex(i);
 			if (level) {
 				bool check = level->bShouldBeLoaded;
-				ImGui::Checkbox(level->PackageName.GetName().c_str(), &check);
+				auto label = level->PackageName.GetName();
+
+				if (level->PackageName.Number > 0) {
+					label += "_" + std::to_string(level->PackageName.Number - 1);
+				}
+
+				ImGui::Checkbox(label.c_str(), &check);
 
 				if (check) {
 					level->bShouldBeLoaded = level->bShouldBeVisible = true;
