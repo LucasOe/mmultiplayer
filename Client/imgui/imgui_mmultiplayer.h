b0d50545e08d74b0d8caa9a2b7dac4cb3155bedd
Toyro98 <teddymolin1@gmail.com>
2024-03-12 15:10:06 +0100

    Added DummyVertical and SmallArrowButton

--- Client/imgui/imgui_mmultiplayer.h
+++ Client/imgui/imgui_mmultiplayer.h
@@ -2,10 +2,13 @@
 
 #include "imgui_internal.h"
 
-namespace ImGui {
-	ImGuiWindow* BeginRawScene(const char *name);
+namespace ImGui 
+{
+	ImGuiWindow* BeginRawScene(const char* name);
 	void EndRawScene();
-	bool Hotkey(const char *label, int *k, const ImVec2 &size_arg = ImVec2(0, 0));
-	void Separator(const float height);
+	bool Hotkey(const char* label, int* k, const ImVec2& size_arg = ImVec2(0, 0));
+	void Separator(const float& height);
 	void HelpMarker(const char* desc);
+	void DummyVertical(const float& height);
+	bool SmallArrowButton(const char* str_id, ImGuiDir dir, ImVec2 size);
 } 
