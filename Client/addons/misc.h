9b71a05e4c59a744273d2fce4db00c51e632eb02
Toyro98 <teddymolin1@gmail.com>
2024-03-12 15:11:05 +0100

    Updated Misc Layout

--- Client/addons/misc.h
+++ Client/addons/misc.h
@@ -5,13 +5,9 @@
 #include "../sdk.h"
 #include <vector>
 
-class Misc : public Addon {
+class Misc : public Addon 
+{
   public:
     bool Initialize();
     std::string GetName();
-};
-
-enum class EOhko : uint8_t {
-    Normal,
-    Extreme,
 };
