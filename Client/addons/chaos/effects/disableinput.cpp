3656b6fcac175e02fe2a430d0a3b6a254b9247f3
Toyro98 <teddymolin1@gmail.com>
2024-06-09 14:16:54 +0200

    Idle Animation
    Plays an idle animation while disable input effect if in right movementstate

--- Client/addons/chaos/effects/disableinput.cpp
+++ Client/addons/chaos/effects/disableinput.cpp
@@ -4,6 +4,9 @@
 
 class DisableInput : public Effect
 {
+private:
+    bool PlayedIdleAnimation = false;
+
 public:
     DisableInput(const std::string& name)
     {
@@ -12,24 +15,62 @@ public:
         DurationType = EDuration::Breif;
     }
 
-    void Start() override {}
+    void Start() override 
+    {
+        PlayedIdleAnimation = false;
+    }
 
     void Tick(const float deltaTime) override
     {
+        const auto pawn = Engine::GetPlayerPawn();
         const auto controller = Engine::GetPlayerController();
+
         controller->bIgnoreButtonInput = TRUE;
         controller->bIgnoreMoveInput = TRUE;
         controller->bIgnoreLookInput = TRUE;
+        controller->CurrentLookAtPoint = nullptr;
+
+        if (PlayedIdleAnimation)
+        {
+            return;
+        }
+
+        if (pawn->MovementState != Classes::EMovement::MOVE_Walking)
+        {
+            return;
+        }
+
+        const auto walking = static_cast<Classes::UTdMove_Walking*>(pawn->Moves[static_cast<size_t>(Classes::EMovement::MOVE_Walking)]);
+        if (!walking)
+        {
+            return;
+        }
+
+        walking->TriggerIdleAnimMinTime = 0.0f;
+        walking->TriggerIdleAnimMaxTime = 0.5f;
+        walking->PlayIdle();
+
+        PlayedIdleAnimation = walking->bIsPlayingIdleAnim ? true : false;
     }
 
     void Render(IDirect3DDevice9* device) override {}
 
     bool Shutdown() override
     {
+        const auto pawn = Engine::GetPlayerPawn();
         const auto controller = Engine::GetPlayerController();
         controller->bIgnoreButtonInput = FALSE;
         controller->bIgnoreMoveInput = FALSE;
         controller->bIgnoreLookInput = FALSE;
+
+        const auto walking = static_cast<Classes::UTdMove_Walking*>(pawn->Moves[static_cast<size_t>(Classes::EMovement::MOVE_Walking)]);
+        if (!walking)
+        {
+            return false;
+        }
+
+        walking->TriggerIdleAnimMinTime = 30.0f;
+        walking->TriggerIdleAnimMaxTime = 40.0f;
 
         return true;
     }
