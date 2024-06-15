9993a067e5a481bafdbcd74625fc176650a29213
Toyro98 <teddymolin1@gmail.com>
2024-06-15 20:05:52 +0200

    Update health.cpp
    Added new health effect

--- Client/addons/chaos/effects/health.cpp
+++ Client/addons/chaos/effects/health.cpp
@@ -5,7 +5,8 @@
 enum class EHealth
 {
     NoHealth,
-    OneHitKnockOut
+    OneHitKnockOut,
+    MaxHealth
 };
 
 class Health : public Effect
@@ -52,6 +53,10 @@ public:
                 pawn->Health = 1;
             }
         }
+        else if (HealthType == EHealth::MaxHealth)
+        {
+            pawn->Health = pawn->MaxHealth;
+        }
     }
 
     void Render(IDirect3DDevice9* device) override {}
@@ -69,6 +74,8 @@ public:
 
 using ZeroHealth = Health;
 using OneHealth = Health;
+using MaxHealth = Health;
 
 REGISTER_EFFECT(ZeroHealth, "Suicide", EHealth::NoHealth);
 REGISTER_EFFECT(OneHealth, "OHKO", EHealth::OneHitKnockOut);
+REGISTER_EFFECT(MaxHealth, "Max Health", EHealth::MaxHealth);
