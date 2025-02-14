3dbaaf95679a901f237f94ec8be19959f030365a
LucasOe <github@lucasoe.com>
2025-02-14 13:31:32 +0100

    Fix 56575c4

--- Client/imgui/imgui_mmultiplayer.h
+++ Client/imgui/imgui_mmultiplayer.h
@@ -4,7 +4,7 @@
 
 namespace ImGui 
 {
-	bool BeginWindow(const char* name, bool* p_open = nullptr, ImGuiWindowFlags flags = 0);
+	bool BeginWindow(const char* name, bool* p_open = NULL, ImGuiWindowFlags flags = 0);
 	ImGuiWindow* BeginRawScene(const char* name, bool saveSettings = false);
 	void EndRawScene();
 	bool Hotkey(const char* label, int* k, const ImVec2& size_arg = ImVec2(0, 0));
